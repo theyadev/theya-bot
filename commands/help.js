e139c965bbdaf57ba52f3952df8dc6773f96487c
theyadev <you@example.com>
2020-01-08 18:59:27 +0100

    Updating and adding Github link

--- commands/help.js
+++ commands/help.js
@@ -2,26 +2,17 @@ module.exports.run = async (message, cooldown) => {
     cooldown.add(message.user.ircUsername)
     setTimeout(function () {
         setTimeout(function () {
-            message.user.sendMessage("Commands List :")
-        }, 0)
-        setTimeout(function () {
-            message.user.sendMessage('!recommend (!r) <mania/osu> = request an osu!/osu!mania map.')
-        }, 500)
-        setTimeout(function () {
-            message.user.sendMessage('!bomb <mania/osu> = request 5 maps.')
-        }, 1000)
-        setTimeout(function () {
-            message.user.sendMessage('!maps = show how many maps there are in the bot.')
-        }, 1500)
-        setTimeout(function () {
-            message.user.sendMessage('!request = request a beatmap to add in the bot.')
-        }, 2000)
+            message.user.sendMessage("Full Commands List : https://github.com/theyadev/theya-bot")
+        }, 0);   
         setTimeout(function () {
             message.user.sendMessage("!mode <mania/osu> = set your default game mode.")
-        }, 2500);
+        }, 500);  
         setTimeout(function () {
-            message.user.sendMessage("!complain = complain about the last map.")
-        }, 3000);
+            message.user.sendMessage('!recommend (!r) <mania/osu> = recommend an osu!/osu!mania map.')
+        }, 1000)
+        setTimeout(function () {
+            message.user.sendMessage('!bomb <mania/osu> = recommend 5 maps.')
+        }, 1500)          
     }, 5000)
 }
 module.exports.help = {
