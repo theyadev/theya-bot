07d7f820a7cc275223a3284b042e290c16ba4799
theyadev <you@example.com>
2020-01-10 22:08:22 +0100

    new maps database

--- commands/maps.js
+++ commands/maps.js
@@ -1,14 +1,9 @@
 const fs = require('fs')
 
 module.exports.run = async (message) => {
-    fs.readFile('./maps/mapsOsu.txt', 'utf8', (err, fileOsu) => {
-        fs.readFile('./maps/mapsMania.txt', 'utf8', (err, fileMania) => {
-            if (err) throw err
-            var ArrayOsu = fileOsu.match(/.{1,7}/g)
-            var ArrayMania = fileMania.match(/.{1,7}/g)
-            message.user.sendMessage(`There are ${ArrayOsu.length} osu! beatmaps & ${ArrayMania.length} osu!mania beatmaps in the bot`)
-        })
-    })
+    let mapsOsu = JSON.parse(fs.readFileSync(`./maps/mapsOsu.json`, "utf8"))        
+    let mapsMania = JSON.parse(fs.readFileSync(`./maps/mapsMania.json`, "utf8"))                  
+            message.user.sendMessage(`There are ${mapsOsu.length} osu! beatmaps & ${mapsMania.length} osu!mania beatmaps in the bot`)   
 }
 module.exports.help = {
     name: "maps"
