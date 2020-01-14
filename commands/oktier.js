aec4c5c8927e481177d7b459aadc69088c4b55c9
theyadev <you@example.com>
2020-01-14 20:08:06 +0100

    Minor change

--- commands/oktier.js
+++ commands/oktier.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const osu = require('node-osu')
-const apiKey = require('C:/Users/Theya/Desktop/theya-bot/secret.js')
-const map = require('C:/Users/Theya/Desktop/theya-bot/mapsDetails.js')
+const { apiKey } = require('../secret.js')
+const map = require('../mapsDetails.js')
 var osuApi = new osu.Api(apiKey, {
   notFoundAsError: true,
   completeScores: false
@@ -11,12 +11,12 @@ module.exports.run = async (message, /*lastMap*/) => {
   fs.readFile(`./maps/mapsGnf.txt`, 'utf8', (err, file) => {
     if (err) throw err
     var Array = file.match(/.{1,7}/g)
-    var randomItem = Array[Math.round(Math.random() * Array.length)]
-    var maps = randomItem - 1
-    console.log(randomItem)
+    var randomMap = Array[Math.round(Math.random() * Array.length)]
+    var maps = randomMap - 1
+    console.log(randomMap)
 
-    osuApi.getBeatmaps({ b: `${randomItem}` }).then(beatmaps => {
-      message.user.sendMessage(`[https://osu.ppy.sh/b/${randomItem} ${beatmaps[0].artist} - ${beatmaps[0].title} [${beatmaps[0].version}]] | ${map.genre(maps)} | ${map.rating(beatmaps[0].difficulty.rating)} ★ | ${map.duree(beatmaps[0].length.total)} ♪ | BPM: ${beatmaps[0].bpm}`)
+    osuApi.getBeatmaps({ b: `${randomMap}` }).then(beatmaps => {
+      message.user.sendMessage(`[https://osu.ppy.sh/b/${randomMap} ${beatmaps[0].artist} - ${beatmaps[0].title} [${beatmaps[0].version}]] | ${map.rating(beatmaps[0].difficulty.rating)} ★ | ${map.duree(beatmaps[0].length.total)} ♪ | BPM: ${beatmaps[0].bpm}`)
     })
   })
 
