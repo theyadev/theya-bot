91d731a5d4552c8df82fd0f274f5a917db1e2658
theyadev <you@example.com>
2020-02-25 18:17:45 +0100

    List of category used in my bot.

---
+++ category.js
@@ -0,0 +1,3 @@
+exports.categoryMania = ["tech", "ln", "sv", "jacks", "classic", "streams"]
+exports.categoryOsu = ["classic", "tech", "alternate", "stream", "jump", "reading"]
+exports.tester = ["Emmanuel", "[-LLENN-]", "MrCookies", "NUL_GNF5", "ChickenWings", "Dono", "nul_tib0t", "Sully_", "BertGry", "ZeroTwo_OwO"]
